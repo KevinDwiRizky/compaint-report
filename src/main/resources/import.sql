d89cb2f7deb35779fe91c24f59b62f2526fb0b31
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2024-06-01 12:09:11 +0700

    import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,26 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+INSERT INTO BidangUnit (bidangUnitName) VALUES
+('Bidang A'),
+('Bidang B'),
+('Bidang C');
+
+
+INSERT INTO RunWorkflow (lastStatus) VALUES
+('Status A'),
+('Status B'),
+('Status C');
+
+
+INSERT INTO HistRunWorkflow (lastStatus) VALUES
+('Historical Status 1'),
+('Historical Status 2'),
+('Historical Status 3');
+
+INSERT INTO Complaint (reportNumber, run_work_flow_id, hist_run_workflow_id) VALUES
+('Report 001', 1, 1),
+('Report 002', 2, 2),
+('Report 003', 3, 3);
+
+INSERT INTO BidangExplain (reportNumber, complaint_id, bidang_unit_id) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3);
